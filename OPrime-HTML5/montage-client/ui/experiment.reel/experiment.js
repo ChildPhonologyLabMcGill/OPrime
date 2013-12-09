3dba7c53fc69745b2ce433e2d5832efef896a612
cesine <cesine@yahoo.com>
2013-12-09 11:40:14 -0500

    numerous attempts at getting the repsonse to be set and show up.'

--- OPrime-HTML5/montage-client/ui/experiment.reel/experiment.js
+++ OPrime-HTML5/montage-client/ui/experiment.reel/experiment.js
@@ -30,10 +30,36 @@ exports.Experiment = Component.specialize( /** @lends Experiment# */ {
 				"responses": [{
 					"reactionTimeAudioOffset": null,
 					"reactionTimeAudioOnset": null,
-					"x": null,
-					"y": null
+					"x": 12,
+					"y": 34
 				}]
 			};
+		}
+	},
+
+	currentResponse: {
+		get: function() {
+
+			var emptyResponse = {
+				"x": 0,
+				"y": 0
+			};
+			if (this._currentStimulus && this._currentStimulus.responses && this._currentStimulus.responses.length > 0) {
+				var response = this._currentStimulus.responses[this._currentStimulus.responses.length - 1];
+				console.log(response);
+				return response;
+			} else {
+				console.log(response);
+				return emptyResponse;
+			}
+		},
+		set: function(value) {
+			if (!this._currentStimulus) {
+				return;
+			}
+
+			this._currentStimulus.responses = this._currentStimulus.responses || [];
+			this._currentStimulus.responses.push(value);
 		}
 	},
 
--- OPrime-HTML5/montage-client/ui/stimulus.reel/one-image-stimulus.reel/one-image-stimulus.js
+++ OPrime-HTML5/montage-client/ui/stimulus.reel/one-image-stimulus.reel/one-image-stimulus.js
@@ -3,17 +3,37 @@
  * @requires montage/ui/component
  */
 var Component = require("montage/ui/component").Component,
-	AbstractStimulus = require("core/abstract-stimulus").AbstractStimulus;
+	AbstractStimulus = require("core/abstract-stimulus").AbstractStimulus,
+	Response = require("ui/response.reel").Response;
 
 /**
  * @class OneImageStimulus
  * @extends AbstractStimulus
  */
-exports.OneImageStimulus = AbstractStimulus.specialize(/** @lends OneImageStimulus# */ {
-    constructor: {
-        value: function OneImageStimulus() {
-            this.super();
-            this.primeImage = "../../../assets/stimuli/image/01_PaulRejointSesAmisAuParcTousLesJours.jpg";
-        }
-    }
+exports.OneImageStimulus = AbstractStimulus.specialize( /** @lends OneImageStimulus# */ {
+	constructor: {
+		value: function OneImageStimulus() {
+			this.super();
+			this.primeImage = "../../../assets/stimuli/image/01_PaulRejointSesAmisAuParcTousLesJours.jpg";
+
+		}
+	},
+	myListProperty: {
+		value: [new Response({
+			"reactionTimeAudioOffset": null,
+			"reactionTimeAudioOnset": null,
+			"x": 132,
+			"y": 334
+		}), new Response({
+			"reactionTimeAudioOffset": null,
+			"reactionTimeAudioOnset": null,
+			"x": 123,
+			"y": 343
+		}), new Response({
+			"reactionTimeAudioOffset": null,
+			"reactionTimeAudioOnset": null,
+			"x": 312,
+			"y": 3423
+		})]
+	}
 });
