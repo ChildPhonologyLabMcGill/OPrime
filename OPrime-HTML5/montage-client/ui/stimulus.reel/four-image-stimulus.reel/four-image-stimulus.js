015adf92b04a070317be243bac73c0644aa266a7
cesine <cesine@yahoo.com>
2013-12-10 00:03:02 -0500

    numerous attempts to listen to a click event on images which count as a response

--- OPrime-HTML5/montage-client/ui/stimulus.reel/four-image-stimulus.reel/four-image-stimulus.js
+++ OPrime-HTML5/montage-client/ui/stimulus.reel/four-image-stimulus.reel/four-image-stimulus.js
@@ -15,5 +15,29 @@ exports.FourImageStimulus = AbstractStimulus.specialize( /** @lends FourImageSti
 			this.super();
 			this.primeImage = "http://fc09.deviantart.net/fs71/f/2012/338/2/1/wat__dash_by_reallyunimportant-d5gl3y0.png";
 		}
+	},
+
+	handleChooseImage1Mousedown: {
+		value: function(e) {
+			console.log("mousedown chooseImage1 child", e);
+		}
+	},
+
+	captureAction: {
+		value: function(e) {
+			console.log("mousedown captue child", e);
+		}
+	},
+
+	handleHover: {
+		value: function(e) {
+			console.log("hover child", e);
+		}
+	},
+
+	captureHover: {
+		value: function(e) {
+			console.log("hover captue child", e);
+		}
 	}
 });
